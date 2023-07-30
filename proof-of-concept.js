48f2b8f6ff8d5c54a33eec97f15d2db26d0e4c98
Ralph Cox <ralphsmail@btinternet.com>
2023-07-30 18:26:42 +0100

    bug fix: add missing revenue variable declaration

--- proof-of-concept.js
+++ proof-of-concept.js
@@ -1,48 +1,49 @@
-const prompt = require('prompt-sync')()
-const moves = ['l', 'h']
-let currentQuarter = 1
-let profit
-let playerMove
+const prompt = require("prompt-sync")();
+const moves = ["l", "h"];
+let currentQuarter = 1;
+let profit;
+let playerMove;
+let revenue;
 
 // main game loop
 do {
-    console.log(`Q${currentQuarter}`)
-    playerMove = getMove()
-    marketMove = getRandomMove()
-    console.log(`Market chose ${marketMove}`)
-    profit = getProfit(playerMove, marketMove)
-    revenue += profit
-    console.log(`Profit: ${profit}`)
-    currentQuarter++
-    process.stdout.write('\n')
-} while (currentQuarter < 5)
+  console.log(`Q${currentQuarter}`);
+  playerMove = getMove();
+  marketMove = getRandomMove();
+  console.log(`Market chose ${marketMove}`);
+  profit = getProfit(playerMove, marketMove);
+  revenue += profit;
+  console.log(`Profit: ${profit}`);
+  currentQuarter++;
+  console.log("\n");
+} while (currentQuarter < 5);
 
 // prompts player for move and cleans input
 function getMove() {
-    let move
-    do {
-        move = prompt('Set price low (l) or high (h): ')
-    } while (!moves.includes(move[0].toLowerCase()))
-    return move
+  let move;
+  do {
+    move = prompt("Set price low (l) or high (h): ");
+  } while (!moves.includes(move[0].toLowerCase()));
+  return move;
 }
 
 // returns market move (h(igh) or l(ow)) at random
 function getRandomMove() {
-    return moves[Math.floor(Math.random() * 2)]
+  return moves[Math.floor(Math.random() * 2)];
 }
 
 // generates payout given player and market moves
 function getProfit(playerMove, marketMove) {
-    let payout
-    if (playerMove === 'h' && marketMove === 'h') {
-        payout = 2000
-    } else if (playerMove === 'l' && marketMove === 'h') {
-        payout = 3000
-    } else if (playerMove === 'l' && marketMove === 'l') {
-        payout = -1000
-    } else {
-        payout = 0
-    }
+  let payout;
+  if (playerMove === "h" && marketMove === "h") {
+    payout = 2000;
+  } else if (playerMove === "l" && marketMove === "h") {
+    payout = 3000;
+  } else if (playerMove === "l" && marketMove === "l") {
+    payout = -1000;
+  } else {
+    payout = 0;
+  }
 
-    return payout
+  return payout;
 }
