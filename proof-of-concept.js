8f2685be1e15effe18c7081d1a4d1080e46978b0
aslaterm32 <aslaterm32@gmail.com>
2023-07-30 12:03:46 +0100

    added proof of concept

---
+++ proof-of-concept.js
@@ -0,0 +1,48 @@
+const prompt = require('prompt-sync')()
+const moves = ['l', 'h']
+let currentQuarter = 1
+let profit
+let playerMove
+
+// main game loop
+do {
+    console.log(`Q${currentQuarter}`)
+    playerMove = getMove()
+    marketMove = getRandomMove()
+    console.log(`Market chose ${marketMove}`)
+    profit = getProfit(playerMove, marketMove)
+    revenue += profit
+    console.log(`Profit: ${profit}`)
+    currentQuarter++
+    process.stdout.write('\n')
+} while (currentQuarter < 5)
+
+// prompts player for move and cleans input
+function getMove() {
+    let move
+    do {
+        move = prompt('Set price low (l) or high (h): ')
+    } while (!moves.includes(move[0].toLowerCase()))
+    return move
+}
+
+// returns market move (h(igh) or l(ow)) at random
+function getRandomMove() {
+    return moves[Math.floor(Math.random() * 2)]
+}
+
+// generates payout given player and market moves
+function getProfit(playerMove, marketMove) {
+    let payout
+    if (playerMove === 'h' && marketMove === 'h') {
+        payout = 2000
+    } else if (playerMove === 'l' && marketMove === 'h') {
+        payout = 3000
+    } else if (playerMove === 'l' && marketMove === 'l') {
+        payout = -1000
+    } else {
+        payout = 0
+    }
+
+    return payout
+}
