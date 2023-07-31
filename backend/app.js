bdc8e1bf7e6c53b3581198fe656b4d6e1b7abd2c
Ralph Cox <ralphsmail@btinternet.com>
2023-07-31 12:03:33 +0100

    can see get result

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-
-const logger = require('./logger')
+const express = require("express");
+const cors = require("cors");
+const logger = require("./logger");
+const results = require("./results");
 
 const app = express();
 
 app.use(cors());
-app.use( express.json())
+app.use(express.json());
 
 app.use(logger);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to game theory API")
-})
+  res.send("Welcome to game theory API");
+});
 
 app.get("/results", (req, res) => {
-  
-})
+  try {
+    res.send(results);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/results", (req, res) => {
+  const newResult = req.body;
+});
 
 module.exports = app;
