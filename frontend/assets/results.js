ec02966dd47dca9f4b7cdb55c971b49f8fd335a8
Cams-Plan <ca.francis@outlook.com>
2023-08-01 22:01:52 +0100

    working on makenewResult funciton and processing time stamp

--- frontend/assets/results.js
+++ frontend/assets/results.js
@@ -1,19 +1,62 @@
-const resultSet = document.querySelector(".result-set");
-const shrunk = document.querySelector(".results-shrunk")
+const resultSection = document.querySelector('.results-section')
+const resultParent = document.querySelector('main')
 
 function cloneResultSet() {
-    const resultSetClone = resultSet.cloneNode(true)
+    const resultSetClone = resultSection.cloneNode(true)
     return resultSetClone
 }
 
+// function makeNewResult(dataFromFetch, index) {
+//   console.log(dataFromFetch)
+//     let cloneData = cloneResultSet()
+//     // console.log(cloneData)
+//     let resu = ""
+//     resu = document.querySelector('main').querySelectorAll(".results-section")
+//     console.log(resu[index])
+//     // resultParent.append(cloneData)
+// }
+
 function makeNewResult(dataFromFetch) {
-  console.log(dataFromFetch)
-    let cloneData = cloneResultSet()
-    console.log(cloneData)
+
+  for (let i = 0; i < dataFromFetch.length; i++){
+    console.log(dataFromFetch[i])
+    let clonedData = cloneResultSet()
+    clonedData.querySelector('.game-num').textContent = `Game ${(dataFromFetch[i].id)+1}`
+    formattedTimestamp = processTimestamp(dataFromFetch[i].timestamp)
+    // clonedData.querySelector('completion-date-time-data').textContent = 0
+  }
+}
+
+function processTimestamp(resultTS) {
+  console.log(resultTS)
+  let year = resultTS.slice(0, )
+  let time = ""
+  
+  console.log(date)
 }
 
 
+// result-id
+
 // document.addEventListener("load", )
+
+// async function getResults() {
+//   let newData = []
+//     try{
+//       const result = await fetch("http://localhost:3001/results")
+//       const data = await result.json()
+//       for (const gameDataset of data) {
+//         newData.push(gameDataset)
+//       }
+//       for (let i = 0; i < newData.length; i++){
+//         makeNewResult(newData[i], i)
+//       }
+
+//     } catch(e) {
+//       console.log(e)
+//     }
+//   }
+
 
 async function getResults() {
   let newData = []
@@ -23,14 +66,11 @@ async function getResults() {
       for (const gameDataset of data) {
         newData.push(gameDataset)
       }
-      for (let i = 0; i < newData.length; i++){
-        makeNewResult(newData[i])
-      }
+      makeNewResult(newData)
 
     } catch(e) {
       console.log(e)
     }
   }
-
 
 getResults();
