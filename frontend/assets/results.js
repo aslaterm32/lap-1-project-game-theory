8e342b1be961382b9eeb749723be2a9fca89b7c4
Cams-Plan <ca.francis@outlook.com>
2023-08-01 14:04:50 +0100

    added async function

--- frontend/assets/results.js
+++ frontend/assets/results.js
@@ -1,5 +1,36 @@
+const resultSet = document.querySelector(".result-set");
+const shrunk = document.querySelector(".results-shrunk")
 
 function cloneResultSet() {
-
+    const resultSetClone = resultSet.cloneNode(true)
 }
 
+function makeNewResult() {
+    cloneResultSet
+}
+
+
+// document.addEventListener("load", )
+
+// async function getResults(){
+//     let resultsList = []
+//     const fetchedResults = await fetch ("http://localhost:3000/results")
+
+//     const result = await fetchedResults.json()
+
+// }
+
+
+// let results = getResults()
+
+
+async function getResults() {
+    const result = await fetch("https://127.0.0.1:3000/results")
+        .then((resp) => {
+            resp.json()
+        })
+        .catch((e) => console.log(e))
+}
+
+let results = getResults()
+console.log(results)
