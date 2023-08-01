e7dd00be2d176843bf1181970160376756576f68
aslaterm32 <aslaterm32@gmail.com>
2023-08-01 11:05:43 +0100

    added post req on frontend, minor changes to post route on app.js

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,64 @@
-const express = require("express");
-const cors = require("cors");
-const logger = require("./logger");
-const results = require("./results");
-const fs = require("fs");
+const express = require('express')
+const cors = require('cors')
+const logger = require('./logger')
+const results = require('./results')
+const fs = require('fs')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use(logger);
+app.use(logger)
 
-app.get("/", (req, res) => {
-  res.send("Welcome to game theory API");
-});
+app.get('/', (req, res) => {
+    res.send('Welcome to game theory API')
+})
 
-app.get("/results", (req, res) => {
-  try {
-    res.send(results);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.post("/results", (req, res) => {
-  const newResult = req.body;
-  //  results.push(req.body);
-  res.status(201).send("Added a post");
-
-  fs.readFile("results.json", "utf8", function readFileCallback(err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      obj = JSON.parse(data); //now it an object
-      obj.push(req.body); //add some data
-      json = JSON.stringify(obj); //convert it back to json
-      fs.writeFile("results.json", json, "utf8", callback); // write it back
+app.get('/results', (req, res) => {
+    try {
+        res.send(results)
+    } catch (error) {
+        next(error)
     }
-  });
-});
+})
 
-function callback(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("File successfully written.");
-  }
-}
-module.exports = app;
+// app.post('/results', (req, res) => {
+//     const newResult = req.body
+//     //  results.push(req.body);
+//     res.status(201).send('Added a post')
+
+//     fs.readFile('results.json', 'utf8', function readFileCallback(err, data) {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             obj = JSON.parse(data) //now it an object
+//             obj.push(req.body) //add some data
+//             json = JSON.stringify(obj) //convert it back to json
+//             fs.writeFile('results.json', json, 'utf8', callback) // write it back
+//         }
+//     })
+// })
+
+app.post('/results', (req, res) => {
+    const newResult = req.body
+    newResult['id'] = results.length
+    results.push(newResult)
+    fs.writeFile('./backend/results.json', JSON.stringify(results), (error) => {
+        if (error) {
+            console.log(error)
+            res.status(500).send('Failed to add result')
+        } else {
+            res.status(201).send('Result added')
+        }
+    })
+})
+
+// function callback(err) {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log('File successfully written.')
+//     }
+// }
+module.exports = app
